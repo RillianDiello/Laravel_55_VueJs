007b110cb20f7773672acbea1aa3314be3260933
Rillian Diello <rilliansf@gmail.com>
2020-06-06 15:43:17 -0400

    personalizaçao de cores do componente vue com o uso da tag style

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,7 +15,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
- Vue.component('topo', require('./components/Topo.vue'));
+Vue.component('topo', require('./components/Topo.vue'));
 Vue.component('pannel', require('./components/Pannel.vue'));
 
 const app = new Vue({
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,17 @@
                 Teste conteudo 0
                 <div class="row">
                     <div class="col-md-4">
-                        <pannel title="Painel 1">
+                        <pannel title="Painel 1" color="blue">
                             Teste conteudo 1
                         </pannel>
                     </div>
                     <div class="col-md-4">
-                        <pannel title="Painel 2">
+                        <pannel title="Painel 2" color="orange">
                             Teste conteudo 2
                         </pannel>
                     </div>
                     <div class="col-md-4">
-                        <pannel title="Painel 3">
+                        <pannel title="Painel 3" color="green">
                             Teste conteudo 3
                         </pannel>
                     </div>
