a6e4070244892524f9f91b65229edff7b1f46a1b
Rillian Diello <rilliansf@gmail.com>
2020-06-06 15:23:00 -0400

    Laravel Start, config de db sqlite, config vuejs

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <pannel title="DashBoard">
+                Teste conteudo 0
+                <div class="row">
+                    <div class="col-md-4">
+                        <pannel title="Painel 1">
+                            Teste conteudo 1
+                        </pannel>
+                    </div>
+                    <div class="col-md-4">
+                        <pannel title="Painel 2">
+                            Teste conteudo 2
+                        </pannel>
+                    </div>
+                    <div class="col-md-4">
+                        <pannel title="Painel 3">
+                            Teste conteudo 3
+                        </pannel>
+                    </div>
+                </div>
+            </pannel>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
